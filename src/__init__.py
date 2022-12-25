d654c23dbf8667463c706779ae3fe6d94cc87ed5
artem-phys <artemkuzmichev.official@gmail.com>
2022-12-25 11:43:04 +0300

    update init

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+from . import qubit_variational_qc, qudit_depolarization_channels, qudit_gates, qudit_variational_qc
+
+__all__ = [
+    "qubit_variational_qc",
+    "qudit_depolarization_channels",
+    "qudit_gates",
+    "qudit_variational_qc",
+]
